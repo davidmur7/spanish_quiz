00b74c7ceb7df5c0aaef362a000c59af62c24ab0
David <davidmurphy1241@gmail.com>
2025-11-11 11:28:23 +0000

    add getWordsByLevel function and use it in beginnerQuiz

--- src/main/kotlin/controllers/WordAPI.kt
+++ src/main/kotlin/controllers/WordAPI.kt
@@ -1,7 +1,6 @@
 package ie.setu.controllers
 import ie.setu.models.Word
 
-import java.io.File
 
 
 class WordAPI () {
@@ -9,7 +8,21 @@ class WordAPI () {
     private val words = ArrayList<Word>()
 
     fun loadWords() {
-        words.add(Word(1, "caminar", "walk", "verb", "beginner", true))
+        words.add(Word(1, "caminar", "walk", "beginner", "verb", true))
+        words.add(Word(2, "comer", "eat", "beginner", "verb", true))
+        words.add(Word(3, "manzana", "apple", "beginner", "noun", true))
+        words.add(Word(4, "fuerte", "strong", "intermediate", "adjective", true))
     }
 
+    fun getWordsByLevel(level: String): List<Word> {
+        val wordsByLevel = ArrayList<Word>()
+        for (word in words) {
+            if (word.level.equals(level)) {
+                wordsByLevel.add(word)
+            }
+        }
+        return wordsByLevel
     }
+
+
+}
